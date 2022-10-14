1f939be408f4820c55fe68a65701338c93ac4a3e
suhas-m-svit <115737219+suhas-m-svit@users.noreply.github.com>
2022-10-14 20:10:40 +0530

    Create queues.java

---
+++ queues.java
@@ -0,0 +1,26 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Main {
+
+    public static void main(String[] args) {
+        // Creating Queue using the LinkedList class
+        Queue<Integer> numbers = new LinkedList<>();
+
+        // offer elements to the Queue
+        numbers.offer(1);
+        numbers.offer(2);
+        numbers.offer(3);
+        System.out.println("Queue: " + numbers);
+
+        // Access elements of the Queue
+        int accessedNumber = numbers.peek();
+        System.out.println("Accessed Element: " + accessedNumber);
+
+        // Remove elements from the Queue
+        int removedNumber = numbers.poll();
+        System.out.println("Removed Element: " + removedNumber);
+
+        System.out.println("Updated Queue: " + numbers);
+    }
+}
