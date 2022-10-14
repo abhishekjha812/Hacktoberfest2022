3b21d291f2990c93e6d2942773cf018ab0dc57ae
D J Ashlin Dev <69051420+Ashlin21dev@users.noreply.github.com>
2022-10-14 22:09:53 +0530

    Added a GUI code in java.

---
+++ GUI_project.java
@@ -0,0 +1,96 @@
+/**
+*Text genereted by Simple GUI Extension for BlueJ
+*/
+import javax.swing.UIManager.LookAndFeelInfo;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import javax.swing.border.Border;
+import javax.swing.*;
+
+
+public class GUI_project extends JFrame {
+
+	private JMenuBar menuBar;
+	private JLabel label2;
+
+	//Constructor 
+	public GUI_project(){
+
+		this.setTitle("GUI_project");
+		this.setSize(500,400);
+		//menu generate method
+		generateMenu();
+		this.setJMenuBar(menuBar);
+
+		//pane with null layout
+		JPanel contentPane = new JPanel(null);
+		contentPane.setPreferredSize(new Dimension(500,400));
+		contentPane.setBackground(new Color(192,192,192));
+
+
+		label2 = new JLabel();
+		label2.setBounds(178,13,90,35);
+		label2.setBackground(new Color(214,217,223));
+		label2.setForeground(new Color(0,0,0));
+		label2.setEnabled(true);
+		label2.setFont(new Font("sansserif",0,12));
+		label2.setText("label");
+		label2.setVisible(true);
+
+		//adding components to contentPane panel
+		contentPane.add(label2);
+
+		//adding panel to JFrame and seting of window position and close operation
+		this.add(contentPane);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.pack();
+		this.setVisible(true);
+	}
+
+	//method for generate menu
+	public void generateMenu(){
+		menuBar = new JMenuBar();
+
+		JMenu file = new JMenu("File");
+		JMenu tools = new JMenu("Tools");
+		JMenu help = new JMenu("Help");
+
+		JMenuItem open = new JMenuItem("Open   ");
+		JMenuItem save = new JMenuItem("Save   ");
+		JMenuItem exit = new JMenuItem("Exit   ");
+		JMenuItem preferences = new JMenuItem("Preferences   ");
+		JMenuItem about = new JMenuItem("About   ");
+
+
+		file.add(open);
+		file.add(save);
+		file.addSeparator();
+		file.add(exit);
+		tools.add(preferences);
+		help.add(about);
+
+		menuBar.add(file);
+		menuBar.add(tools);
+		menuBar.add(help);
+	}
+
+
+
+	 public static void main(String[] args){
+		System.setProperty("swing.defaultlaf", "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new GUI_project();
+			}
+		});
+	}
+
+}
