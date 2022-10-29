c2e175cb0886f8e3723f62cad5394da0c7968753
Divya Chakshu Bisht <chakshubisht459@gmail.com>
2022-10-29 19:08:38 +0530

    Whatsapp Project added Using flutter

---
+++ Whatsapp Clone/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:camera/camera.dart';
+import 'package:chatapp/Screens/CameraScreen.dart';
+import 'package:chatapp/Screens/Homescreen.dart';
+import 'package:chatapp/Screens/LoginScreen.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          fontFamily: "OpenSans",
+          primaryColor: Color(0xFF075E54),
+          accentColor: Color(0xFF128C7E)),
+      home: LoginScreen(),
+    );
+  }
+}
